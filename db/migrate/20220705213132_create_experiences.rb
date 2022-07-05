72ca60e90792243f60aa6d8b2c71d877fb57d180
charles geoffroy <charles@dgtlpay.co>
2022-07-05 22:33:45 +0100

    creation model experience

---
+++ db/migrate/20220705213132_create_experiences.rb
@@ -0,0 +1,16 @@
+class CreateExperiences < ActiveRecord::Migration[7.0]
+  def change
+    create_table :experiences do |t|
+      t.string :name
+      t.string :type
+      t.string :address
+      t.text :description
+      t.integer :max_ppl
+      t.integer :price
+      t.string :email
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
