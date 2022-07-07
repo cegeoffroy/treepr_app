90d953e8c19e6eb185d3025e1f4d6ff9b9cdbc1e
charles geoffroy <charles@dgtlpay.co>
2022-07-08 00:25:26 +0100

    updated user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating users...'
+admin = User.new
+admin.first_name = "Admin"
+admin.last_name = "Boss"
+admin.email = 'charles@treepr.co.uk'
+admin.password = "123456"
+admin.password_confirmation = "123456"
+admin.admin = true
+admin.save!
+
+charles = User.new
+charles.first_name = "charles"
+charles.last_name = "geoffroy"
+charles.email = 'charles@dgtlpay.co'
+charles.password = "123456"
+charles.password_confirmation = "123456"
+charles.save!
+
+puts 'Finished creating users!'
